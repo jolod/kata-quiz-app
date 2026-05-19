3c5cb6435a57ae8dc2979be1e0932826d26b000f
Johan Lodin <jolod@users.noreply.github.com>
2026-05-19 22:05:14 +0700

    Record solution.

--- src/main/java/se/jolod/Question.java
+++ src/main/java/se/jolod/Question.java
@@ -1,9 +1,26 @@
 package se.jolod;
 
-public class Question {
-    public final String description;
+import java.util.ArrayList;
+import java.util.List;
 
-    Question(String description) {
-        this.description = description;
+public record Question(String description, List<String> lines) {
+    public static Question trueOrFalse(String description) {
+        return new Question(description, renderOptions(List.of("True", "False")));
+    }
+
+    public static Question multipleChoice(String description, List<String> options) {
+        return new Question(description, renderOptions(options));
+    }
+
+    public static Question freeText(String description) {
+        return new Question(description, List.of("Answer: ___________________"));
+    }
+
+    private static ArrayList<String> renderOptions(List<String> options) {
+        var lines = new ArrayList<String>();
+        for (int i = 0; i < options.size(); i++) {
+            lines.add("%d. %s".formatted(i + 1, options.get(i)));
+        }
+        return lines;
     }
 }
