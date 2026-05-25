8a73e5389c6576dd8b75136352013faa6be4375f
Johan Lodin <jolod@users.noreply.github.com>
2026-05-25 23:13:49 +0700

    Refactor each print function as private static methods.

--- src/main/java/se/jolod/Main.java
+++ src/main/java/se/jolod/Main.java
@@ -16,21 +16,34 @@ public class Main {
     public static void printQuiz(List<Question> questions) {
         for (var question : questions) {
             System.out.println(question.description);
-            switch (question) {
-                case BooleanQuestion _ -> {
-                    System.out.println("1. True");
-                    System.out.println("2. False");
-                }
-                case MultipleChoiceQuestion multipleChoiceQuestion -> {
-                    var options = multipleChoiceQuestion.options;
-                    for (int i = 0; i < options.size(); i++) {
-                        System.out.printf("%d. %s%n", i + 1, options.get(i));
-                    }
-                }
-                case TextQuestion _ -> System.out.println("Answer: ___________________");
-                default -> throw new RuntimeException();
-            }
+            printQuestionChoices(question);
             System.out.println();
         }
+    }
+
+    private static void printQuestionChoices(Question question) {
+        switch (question) {
+            case BooleanQuestion _ -> printBooleanQuestionChoices();
+            case MultipleChoiceQuestion multipleChoiceQuestion ->
+                    printMultipleChoiceQuestionChoices(multipleChoiceQuestion);
+            case TextQuestion _ -> printTextQuestionChoices();
+            default -> throw new RuntimeException();
+        }
+    }
+
+    private static void printBooleanQuestionChoices() {
+        System.out.println("1. True");
+        System.out.println("2. False");
+    }
+
+    private static void printMultipleChoiceQuestionChoices(MultipleChoiceQuestion multipleChoiceQuestion) {
+        var options = multipleChoiceQuestion.options;
+        for (int i = 0; i < options.size(); i++) {
+            System.out.printf("%d. %s%n", i + 1, options.get(i));
+        }
+    }
+
+    private static void printTextQuestionChoices() {
+        System.out.println("Answer: ___________________");
     }
 }
